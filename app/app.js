95097a02309de22215803b99e127fc4f69dece1e
minustehbare <minustehbare@gmail.com>
2016-01-05 12:44:24 -0500

    Address deprecation warnings emitted from express-session

--- app/app.js
+++ app/app.js
@@ -29,7 +29,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({ 'secret': process.env.SESSION_SECRET }));
+app.use(session({
+  'secret': process.env.SESSION_SECRET,
+  'resave': false,
+  'saveUninitialized': false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(passport.session());
